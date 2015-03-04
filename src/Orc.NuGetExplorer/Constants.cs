1a8f4addee427555439f17369b3a9c7f851069ef
Maxim Khomutov <mkhomutov@gmail.com>
2015-03-04 13:50:30 +0300

    ORCOMP-61 fixed compile error

--- src/Orc.NuGetExplorer/Constants.cs
+++ src/Orc.NuGetExplorer/Constants.cs
@@ -3,16 +3,22 @@
 //   Copyright (c) 2008 - 2015 Wild Gums. All rights reserved.
 // </copyright>
 // --------------------------------------------------------------------------------------------------------------------
+
+
 namespace Orc.NuGetExplorer
 {
     public static class RepoName
     {
+        #region Fields
         public const string All = "All";
+        #endregion
     }
 
     public class Settings
     {
+        #region Fields
         public const string DestFolder = "DestFolder";
         public const string PackageSources = "PackageSources";
+        #endregion
     }
 }
