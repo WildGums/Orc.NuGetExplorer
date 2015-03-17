e29af79defb12529a698ab75ccfd8b952a0fa66b
Maxim Khomutov <mkhomutov@gmail.com>
2015-03-17 12:46:07 +0400

    ORCOMP-86 added default images support (if package has no icon)

---
+++ src/Orc.NuGetExplorer/Converters/PackageImageConverter.cs
@@ -0,0 +1,29 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="PackageImageConverter.cs" company="Wild Gums">
+//   Copyright (c) 2008 - 2015 Wild Gums. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+
+namespace Orc.NuGetExplorer.Converters
+{
+    using System;
+    using System.Windows.Media.Imaging;
+    using Catel.MVVM.Converters;
+
+    public class PackageImageConverter : ValueConverterBase
+    {
+        #region Methods
+        protected override object Convert(object value, Type targetType, object parameter)
+        {
+            var uri = value as Uri;
+            if (uri == null)
+            {
+                uri = new Uri("http://nuget.org/Content/Images/packageDefaultIcon.png");
+            }
+
+            return new BitmapImage(uri);
+        }
+        #endregion
+    }
+}
