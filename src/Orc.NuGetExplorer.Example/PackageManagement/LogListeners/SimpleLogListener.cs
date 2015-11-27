659aabb557218755938708626e603559b7992e82
Geert van Horrik <geert@catenalogic.com>
2015-11-27 12:36:13 +0100

    Updated to Catel 4.4

--- src/Orc.NuGetExplorer.Example/PackageManagement/LogListeners/SimpleLogListener.cs
+++ src/Orc.NuGetExplorer.Example/PackageManagement/LogListeners/SimpleLogListener.cs
@@ -36,22 +36,22 @@ namespace Orc.NuGetExplorer.Example
         #region Methods
         protected override void OnInfo(object sender, NuGetLogRecordEventArgs e)
         {
-            _dispatcherService.Invoke(() => _echo.Lines.Add(string.Format("Info: {0}", e.Message)));
+            _dispatcherService.Invoke(() => _echo.Lines.Add(string.Format("Info: {0}", e.Message)), true);
         }
 
         protected override void OnError(object sender, NuGetLogRecordEventArgs e)
         {
-            _dispatcherService.Invoke(() => _echo.Lines.Add(string.Format("Error: {0}", e.Message)));
+            _dispatcherService.Invoke(() => _echo.Lines.Add(string.Format("Error: {0}", e.Message)), true);
         }
 
         protected override void OnDebug(object sender, NuGetLogRecordEventArgs e)
         {
-            _dispatcherService.Invoke(() => _echo.Lines.Add(string.Format("Debug: {0}", e.Message)));
+            _dispatcherService.Invoke(() => _echo.Lines.Add(string.Format("Debug: {0}", e.Message)), true);
         }
 
         protected override void OnWarning(object sender, NuGetLogRecordEventArgs e)
         {
-            _dispatcherService.Invoke(() => _echo.Lines.Add(string.Format("Warning: {0}", e.Message)));
+            _dispatcherService.Invoke(() => _echo.Lines.Add(string.Format("Warning: {0}", e.Message)), true);
         }
         #endregion
     }
