8db6244f821e34bd717abb172ae94f193fbd24e2
Artem Saikin <art.saikin@gmail.com>
2019-11-26 19:22:51 +0400

    packages batch for gum

---
+++ src/Orc.NuGetExplorer.Xaml/Models/PackagesBatch.cs
@@ -0,0 +1,27 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="PackagesBatch.cs" company="WildGums">
+//   Copyright (c) 2008 - 2015 WildGums. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+
+namespace Orc.NuGetExplorer
+{
+    using System.Collections.ObjectModel;
+    using Catel.Collections;
+
+    public class PackagesBatch
+    {
+        #region Constructors
+        public PackagesBatch()
+        {
+            PackageList = new FastObservableCollection<IPackageDetails>();
+        }
+        #endregion
+
+        #region Properties
+        public ObservableCollection<IPackageDetails> PackageList { get; set; }
+        public PackageOperationType OperationType { get; set; }
+        #endregion
+    }
+}
