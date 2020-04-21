967209329496d8b75a1553ca49a0d007496ce6eb
Artem Saikin <art.saikin@gmail.com>
2020-04-21 20:49:57 +0400

    ORCOMP-477 Show latest available versions on Update tab add more informative tooltips for all version text on packages

--- src/Orc.NuGetExplorer.Xaml/ViewModels/PageItemViewModel.cs
+++ src/Orc.NuGetExplorer.Xaml/ViewModels/PageItemViewModel.cs
@@ -14,15 +14,13 @@
     internal class PageItemViewModel : ViewModelBase
     {
         private static readonly ILog Log = LogManager.GetCurrentClassLogger();
+        private static readonly string InstalledVersionText = "Installed version";
+        private static readonly string LastVersionText = "Latest version";
+        private static readonly string UpdateVersionText = "Update version";
 
-        private readonly IRepositoryContextService _repositoryService;
-
-        public PageItemViewModel(NuGetPackage package, IRepositoryContextService repositoryService)
+        public PageItemViewModel(NuGetPackage package)
         {
             Argument.IsNotNull(() => package);
-            Argument.IsNotNull(() => repositoryService);
-
-            _repositoryService = repositoryService;
 
             Package = package;
 
@@ -38,6 +36,7 @@
         [Expose("Authors")]
         [Expose("IconUrl")]
         [Expose("Identity")]
+        [Expose("FromPage")]
         public NuGetPackage Package { get; set; }
 
         [ViewModelToModel]
@@ -50,11 +49,13 @@
 
         public bool CanBeAddedInBatchOperation { get; set; }
 
-        public bool IsSecondaryVersionShowed { get; private set; } = false;
-
-        public NuGetVersion FirstVersion { get; set; }
+        public NuGetVersion PrimaryVersion { get; set; }
 
         public NuGetVersion SecondaryVersion { get; set; }
+
+        public string PrimaryVersionDescription { get; set; }
+
+        public string SecondaryVersionDescription { get; set; }
 
         public Command<MouseButtonEventArgs> CheckItem { get; set; }
 
@@ -75,33 +76,53 @@
             IsDownloadCountShowed = packageOrigin == MetadataOrigin.Browse;
             CanBeAddedInBatchOperation = packageOrigin == MetadataOrigin.Updates;
 
-            FirstVersion = Package.Identity.Version;
-
-            switch (packageOrigin)
-            {
-                case MetadataOrigin.Browse:
-                    IsSecondaryVersionShowed = true;
-                    SecondaryVersion = Package.InstalledVersion;
-                    break;
-
-                case MetadataOrigin.Updates:
-                    IsSecondaryVersionShowed = true;
-                    FirstVersion = Package.InstalledVersion;
-                    SecondaryVersion = Package.LastVersion;
-                    break;
-            }
+            GetPrimaryVersionInfo(packageOrigin, Package);
+            GetSecondaryVersionInfo(packageOrigin, Package);
 
             return base.InitializeAsync();
         }
 
         protected override void OnModelPropertyChanged(object sender, PropertyChangedEventArgs e)
         {
-            if (string.Equals(e.PropertyName, nameof(Package.InstalledVersion)) && Package.FromPage == MetadataOrigin.Browse)
+            if (string.Equals(e.PropertyName, nameof(Package.InstalledVersion)))
             {
-                SecondaryVersion = Package.InstalledVersion;
+                GetSecondaryVersionInfo(Package.FromPage, Package);
             }
 
             base.OnModelPropertyChanged(sender, e);
+        }
+
+        private void GetSecondaryVersionInfo(MetadataOrigin fromPage, NuGetPackage package)
+        {
+            if (MetadataOrigin.Browse == fromPage)
+            {
+                SecondaryVersion = package.InstalledVersion;
+                SecondaryVersionDescription = $"{InstalledVersionText}: {SecondaryVersion}";
+                return;
+            }
+
+            SecondaryVersion = package.LastVersion;
+
+            if (MetadataOrigin.Updates == fromPage)
+            {
+                SecondaryVersionDescription = $"{UpdateVersionText}: {SecondaryVersion}";
+                return;
+            }
+
+            SecondaryVersionDescription = $"{LastVersionText}: {SecondaryVersion}";
+        }
+
+        private void GetPrimaryVersionInfo(MetadataOrigin fromPage, NuGetPackage package)
+        {
+            if (MetadataOrigin.Browse == fromPage)
+            {
+                PrimaryVersion = package.Identity.Version;
+                PrimaryVersionDescription = $"{LastVersionText}: {PrimaryVersion}";
+                return;
+            }
+
+            PrimaryVersion = package.InstalledVersion;
+            PrimaryVersionDescription = $"{InstalledVersionText}: {PrimaryVersion}";
         }
     }
 }
