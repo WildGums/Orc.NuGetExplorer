63debe0a060e98fe7801135ba78abebbef8d82d6
Geert van Horrik <geert@catenalogic.com>
2020-05-08 17:06:23 +0200

    Disable WpfAnalyzers for now

--- src/SolutionAssemblyInfo.cs
+++ src/SolutionAssemblyInfo.cs
@@ -6,7 +6,8 @@
 using System.Reflection;
 
 [assembly: AssemblyCompany("WildGums")]
-[assembly: AssemblyVersion("4.0.0")]
-[assembly: AssemblyFileVersion("4.0.0")]
-[assembly: AssemblyInformationalVersion("4.0.0-alpha.1")]
-[assembly: AssemblyCopyright("Copyright © WildGums 2014 - 2019")]
+[assembly: AssemblyVersion("4.1.0")]
+[assembly: AssemblyFileVersion("4.1.0")]
+[assembly: AssemblyInformationalVersion("4.1.0-alpha.707")]
+[assembly: AssemblyCopyright("Copyright © WildGums 2014 - 2020")]
+
