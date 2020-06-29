29e7e62258f64dcc01ea216e0aea4c05802361dc
Artem Saikin <art.saikin@gmail.com>
2020-06-29 10:27:11 +0400

    Fix first-chance NotSupportedException

---
+++ src/Orc.NuGetExplorer.Xaml/Converters/ExtendedUriToStringConverter.cs
@@ -0,0 +1,23 @@
+﻿namespace Orc.NuGetExplorer.Converters
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Catel.MVVM.Converters;
+
+    public class ExtendedUriToStringConverter : ValueConverterBase<Uri, string>
+    {
+        protected override object Convert(Uri value, Type targetType, object parameter)
+        {
+            if (value is null)
+            {
+                return string.Empty;
+            }
+
+            return value.ToString();
+        }
+    }
+}
