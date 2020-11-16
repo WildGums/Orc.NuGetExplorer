b9406d6be7f0788e0dd3f4f90a16b11fdf8e18a1
Artem Saikin <art.saikin@gmail.com>
2020-11-17 01:50:37 +0400

    ORCOMP-573 Fix pick all packages label click

--- src/Orc.NuGetExplorer.Xaml/Views/PageActionBar.xaml.cs
+++ src/Orc.NuGetExplorer.Xaml/Views/PageActionBar.xaml.cs
@@ -2,19 +2,11 @@
 {
     using Catel.Windows.Controls;
 
-    /// <summary>
-    /// Interaction logic for PageActionBar.xaml
-    /// </summary>
     internal partial class PageActionBar : UserControl
     {
         public PageActionBar()
         {
             InitializeComponent();
-        }
-
-        private void OnSelectAllPackagesTextBlockMouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
-        {
-            CheckAllBox.SetCurrentValue(System.Windows.Controls.Primitives.ToggleButton.IsCheckedProperty, !CheckAllBox.IsChecked);
         }
     }
 }
