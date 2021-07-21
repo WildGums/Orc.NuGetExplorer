7c0335aa3a997b538571b1e4c99c8409a826f828
Artem Saikin <art.saikin@gmail.com>
2021-07-21 18:23:55 +0300

    Optimizations for install process

--- src/Orc.NuGetExplorer/NuGet/Resources/DependencyInfoResourceCollection.cs
+++ src/Orc.NuGetExplorer/NuGet/Resources/DependencyInfoResourceCollection.cs
@@ -7,9 +7,11 @@
     using System.Threading.Tasks;
     using Catel;
     using global::NuGet.Protocol.Core.Types;
+    using MethodTimer;
     using NuGet.Common;
     using NuGet.Frameworks;
     using NuGet.Packaging.Core;
+    using NuGet.Versioning;
 
     /// <summary>
     /// Wrapper against list of dependency recources from multiple repositories
@@ -40,9 +42,24 @@
             Argument.IsNotNull(() => resource);
 
             _resources = new List<DependencyInfoResource>
-            { 
-                resource 
+            {
+                resource
             };
+        }
+
+        public async Task<IEnumerable<SourcePackageDependencyInfo>> ResolvePackagesWithVersionSatisfyRangeAsync(PackageIdentity package, VersionRange versionRange, NuGetFramework projectFramework, SourceCacheContext cacheContext,
+            ILogger log, CancellationToken token)
+        {
+            var singlePackage = await ResolvePackageAsync(package, projectFramework, cacheContext, log, token);
+
+            // Check is this package satisfy requirements, if not, retrieve all dependency infos and find required package
+            if (singlePackage is not null && versionRange.Satisfies(singlePackage.Version))
+            {
+                return new[] { singlePackage };
+            }
+
+            var packagesInRange = (await ResolvePackagesAsync(package, projectFramework, cacheContext, log, token)).Where(package => versionRange.Satisfies(package.Version)).ToList();
+            return packagesInRange;
         }
 
         public async Task<SourcePackageDependencyInfo> ResolvePackageAsync(PackageIdentity package, NuGetFramework projectFramework, SourceCacheContext cacheContext, ILogger log, CancellationToken token)
@@ -61,6 +78,7 @@
             return null;
         }
 
+        [Time]
         public async Task<IEnumerable<SourcePackageDependencyInfo>> ResolvePackagesAsync(PackageIdentity package, NuGetFramework projectFramework, SourceCacheContext cacheContext, ILogger log, CancellationToken token)
         {
             HashSet<SourcePackageDependencyInfo> packageDependencyInfos = new HashSet<SourcePackageDependencyInfo>();
