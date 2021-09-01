8fb73e05d8d7455e67201e972558e92d680d184c
Artem Saikin <art.saikin@gmail.com>
2021-09-01 12:11:57 +0300

    Add descriptions to FeedVerificationResult

--- src/Orc.NuGetExplorer/Services/Enums/FeedVerificationResult.cs
+++ src/Orc.NuGetExplorer/Services/Enums/FeedVerificationResult.cs
@@ -1,18 +1,18 @@
-﻿// --------------------------------------------------------------------------------------------------------------------
-// <copyright file="FeedVerificationResult.cs" company="WildGums">
-//   Copyright (c) 2008 - 2015 WildGums. All rights reserved.
-// </copyright>
-// --------------------------------------------------------------------------------------------------------------------
-
-
-namespace Orc.NuGetExplorer
+﻿namespace Orc.NuGetExplorer
 {
+    using System.ComponentModel;
+
     public enum FeedVerificationResult
     {
+        [Description("The feed isn't verified yet")]
         Unknown,
+        [Description("The feed is verified and valid")]
         Valid,
+        [Description("The feed requires authentication")]
         AuthenticationRequired,
+        [Description("Access to feed denied")]
         AuthorizationRequired,
+        [Description("The feed is verified but invalid")]
         Invalid
     }
 }
